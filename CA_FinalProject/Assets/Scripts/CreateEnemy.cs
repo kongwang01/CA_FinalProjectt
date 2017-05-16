945bafd8aff16697499e4b1517c991d7d69e581f
kongwang01 <kongwang01@users.noreply.github.com>
2017-05-16 10:56:31 +0800

    can auto move to player

---
+++ CA_FinalProject/Assets/Scripts/CreateEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateEnemy : MonoBehaviour {
+    public GameObject player;
+    public GameObject Enemy;
+
+	// Use this for initialization
+	void Start () {
+        //Enemy.gameObject.transform.localScale -= new Vector3(501, 501, 501);
+        GameObject newEnemy = Instantiate(Enemy);
+        newEnemy.transform.Translate(5, 0, 5);
+
+        //GameObject objState = GameObject.Find("MoveToPlayer");
+        //newEnemy.AddComponent(Type.GetType("MoveToPlayer"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
